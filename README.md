b3f7c814ddc1aa83a5c8190f5c225411a8185ea0
Kasper Løhde <kasperloehde@gmail.com>
2022-01-21 20:23:19 +0100

    added MsgPackRaw

--- README.md
+++ README.md
@@ -1,12 +1,79 @@
 # axum-msgpack
 
-`axum-msgpack` 
+`axum-msgpack` adds MessagePack features to axum.
 
-# TODO 
-- Write docs 
-- Make test
-- Figure out how to publish a crate
-- Enjoy!
+[![Documentation](https://docs.rs/axum-msgpack/badge.svg)](https://docs.rs/axum-msgpack)
+[![Crates.io](https://img.shields.io/crates/v/axum-msgpack)](https://crates.io/crates/axum-msgpack)
+
+More information about this crate can be found in the [crate documentation][docs].
+
+[serde][serde]
+[msgpack][msgpack]
+
+## Features
+* Serialize, Deserialize MessagePack from request/response 
+
+
+## Usage example
 
 ```rust
+use axum_msgpack::MsgPack;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    pub name: String,
+    #[serde(with = "serde_bytes")]
+    pub data: Vec<u8>
+}
+
+// axum handler | MsgPack
+async fn get_handler() -> MsgPack<User> {
+    let user = User {
+        name: "steve".to_string(),
+        data: vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    };
+    MsgPack(user)
+}
+
+// axum handler | MsgPack
+async fn post_handler(MsgPack(user): MsgPack<User>) -> Html<String> {
+    let string = format!("<h1>{:?}</h1>", user.name);
+    Html(string)
+}
+
+// axum handler | MsgPackRaw
+async fn get_handler_raw() -> MsgPackRaw<User> {
+    let user = User {
+        name: "steve".to_string(),
+        data: vec![0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    };
+    MsgPack(user)
+}
+
+// axum handler | MsgPackRaw
+async fn post_handler_raw(MsgPackRaw(user): MsgPackRaw<User>) -> Html<String> {
+    let string = format!("<h1>{:?}</h1>", user.name);
+    Html(string)
+}
 ```
+
+Dependencies for serializing/deserializing MsgPack
+```toml
+serde = { version = "1.0", features = ["derive"] }
+serde_bytes = "0.11"
+```
+
+In order to pack arrays correct remember to add `#[serde(with = "serde_bytes")]` to the struct member.
+
+
+## Safety
+This crate uses #![forbid(unsafe_code)] to ensure everything is implemented in 100% safe Rust.
+
+## License
+This project is licensed under the MIT license.
+
+
+[docs]: https://docs.rs/axum-msgpack
+[serde]: https://serde.rs/
+[msgpack]: https://msgpack.org/
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![forbid(unsafe_code)]
 use std::ops::{Deref, DerefMut};
 
 use axum::{
@@ -166,6 +167,152 @@ where
     }
 }
 
+/// MessagePack Extractor / Response.
+///
+/// When used as an extractor, it can deserialize request bodies into some type that
+/// implements [`serde::Deserialize`]. If the request body cannot be parsed, or value of the
+/// `Content-Type` header does not match any of the `application/msgpack`, `application/x-msgpack`
+/// or `application/*+msgpack` it will reject the request and return a `400 Bad Request` response.
+///
+/// # Extractor example
+///
+/// ```rust,no_run
+/// use axum::{
+///     routing::post,
+///     Router,
+/// };
+/// use axum_msgpack::MsgPackRaw;
+/// use serde::Deserialize;
+///
+/// #[derive(Deserialize)]
+/// struct CreateUser {
+///     email: String,
+///     password: String,
+/// }
+///
+/// async fn create_user(MsgPackRaw(payload): MsgPackRaw<CreateUser>) {
+///     // payload is a `CreateUser`
+/// }
+///
+/// let app = Router::new().route("/users", post(create_user));
+/// # async {
+/// # axum::Server::bind(&"".parse().unwrap()).serve(app.into_make_service()).await.unwrap();
+/// # };
+/// ```
+///
+/// When used as a response, it can serialize any type that implements [`serde::Serialize`] to
+/// `MsgPackRaw`, and will automatically set `Content-Type: application/msgpack` header.
+///
+/// # Response example
+///
+/// ```
+/// use axum::{
+///     extract::Path,
+///     routing::get,
+///     Router,
+/// };
+/// use axum_msgpack::MsgPackRaw;
+/// use serde::Serialize;
+/// use uuid::Uuid;
+///
+/// #[derive(Serialize)]
+/// struct User {
+///     id: Uuid,
+///     username: String,
+/// }
+///
+/// async fn get_user(Path(user_id) : Path<Uuid>) -> MsgPackRaw<User> {
+///     let user = find_user(user_id).await;
+///     MsgPackRaw(user)
+/// }
+///
+/// async fn find_user(user_id: Uuid) -> User {
+///     // ...
+///     # unimplemented!()
+/// }
+///
+/// let app = Router::new().route("/users/:id", get(get_user));
+/// # async {
+/// # axum::Server::bind(&"".parse().unwrap()).serve(app.into_make_service()).await.unwrap();
+/// # };
+/// # mod uuid {
+/// # use serde::{Serialize, Deserialize};
+/// # #[derive(Serialize, Deserialize)]
+/// # pub struct Uuid;
+/// # }
+/// ```
+#[derive(Debug, Clone, Copy, Default)]
+pub struct MsgPackRaw<T>(pub T);
+
+#[async_trait]
+impl<T, B> FromRequest<B> for MsgPackRaw<T>
+where
+    T: DeserializeOwned,
+    B: axum::body::HttpBody + Send,
+    B::Data: Send,
+    B::Error: Into<BoxError>,
+{
+    type Rejection = MsgPackRejection;
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        if message_pack_content_type(req)? {
+            let bytes = Bytes::from_request(req).await?;
+            let value = rmp_serde::from_read_ref(&bytes).map_err(InvalidMsgPackBody::from_err)?;
+
+            Ok(MsgPackRaw(value))
+        } else {
+            Err(MissingMsgPackContentType.into())
+        }
+    }
+}
+
+impl<T> Deref for MsgPackRaw<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for MsgPackRaw<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> From<T> for MsgPackRaw<T> {
+    fn from(inner: T) -> Self {
+        Self(inner)
+    }
+}
+
+impl<T> IntoResponse for MsgPackRaw<T>
+where
+    T: Serialize,
+{
+    fn into_response(self) -> Response {
+        let bytes = match rmp_serde::encode::to_vec(&self.0) {
+            Ok(res) => res,
+            Err(err) => {
+                return Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .header(header::CONTENT_TYPE, "text/plain")
+                    .body(body::boxed(Full::from(err.to_string())))
+                    .unwrap();
+            }
+        };
+        
+        let mut res = bytes.into_response();
+
+        res.headers_mut().insert(
+            header::CONTENT_TYPE,
+            HeaderValue::from_static("application/msgpack"),
+        );
+        res
+    }
+}
+
+
 fn message_pack_content_type<B>(req: &RequestParts<B>) -> Result<bool, HeadersAlreadyExtracted> {
     let content_type = if let Some(content_type) = req
         .headers()
@@ -204,13 +351,21 @@ mod tests {
 
     use serde::{Serialize, Deserialize};
     use hyper::{body::to_bytes, Request, header};
-    use crate::{MsgPack, MsgPackRejection};
+    use crate::{MsgPack, MsgPackRaw, MsgPackRejection};
 
     #[derive(Debug, Serialize, Deserialize, PartialEq)]
     struct Input { foo: String }
     
     fn into_request<T: Serialize>(value: &T) -> Request<Body> {
         let serialized = rmp_serde::encode::to_vec_named(&value)
+            .expect("Failed to serialize test struct");
+
+        let body = Body::from(serialized);
+        Request::new(body) 
+    }
+
+    fn into_request_raw<T: Serialize>(value: &T) -> Request<Body> {
+        let serialized = rmp_serde::encode::to_vec(&value)
             .expect("Failed to serialize test struct");
 
         let body = Body::from(serialized);
@@ -242,6 +397,37 @@ mod tests {
             .insert(header::CONTENT_TYPE, HeaderValue::from_static("application/msgpack"));
 
         let outcome = <MsgPack<Input> as FromRequest<_>>::from_request(&mut RequestParts::new(request)).await;
+        assert!(outcome.is_ok());
+        let outcome = outcome.unwrap();
+
+        assert_eq!(input, outcome.0);
+    }
+
+    #[tokio::test]
+    async fn serializes_raw() {
+        let input = Input { foo: "bar".into()};
+        let serialized = rmp_serde::encode::to_vec(&input);
+        assert!(serialized.is_ok());
+        let serialized = serialized.unwrap();
+        
+        let body = MsgPackRaw(input).into_response().into_body();
+        let bytes = to_bytes(body).await;
+        assert!(bytes.is_ok());
+        let bytes = bytes.unwrap();
+
+        assert_eq!(serialized, bytes);
+    }
+
+    #[tokio::test]
+    async fn deserializes_raw() {
+        let input = Input { foo: "bar".into()};
+        let mut request = into_request_raw(&input);
+
+        request
+            .headers_mut()
+            .insert(header::CONTENT_TYPE, HeaderValue::from_static("application/msgpack"));
+
+        let outcome = <MsgPackRaw<Input> as FromRequest<_>>::from_request(&mut RequestParts::new(request)).await;
         assert!(outcome.is_ok());
         let outcome = outcome.unwrap();
 
